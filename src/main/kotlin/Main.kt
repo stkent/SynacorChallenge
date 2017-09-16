0c6408d0ec656e12882ba204b4c4f52be7be5d1d
Stuart Kent <skent.dev@gmail.com>
2017-09-15 20:39:18 -0400

    Misc cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,11 +10,11 @@ object Main {
     fun main(args: Array<String>) {
         val programBytes = readProgramBytes("challenge.bin")
 
-        println("""
+        print("""
             Choose an option:
 
-            [1] Run program")
-            [2] Decompile program")
+            [1] Run program"
+            [2] Decompile program"
 
             > """.trimIndent())
 
@@ -22,7 +22,7 @@ object Main {
 
         when (option) {
             "1" -> VM(actor = InteractiveActor).runProgram(programBytes)
-            "2" -> Decompiler().decompile(programBytes)
+            "2" -> Decompiler().decompile(programBytes, "tempfile.txt")
             else -> println("Option not recognized. Exiting.")
         }
     }
--- src/main/kotlin/vm/Decompiler.kt
+++ src/main/kotlin/vm/Decompiler.kt
@@ -1,37 +1,39 @@
 package vm
 
+import java.io.File
+
 class Decompiler {
 
-    fun decompile(programBytes: ByteArray) {
-        val intInstructions = parseIntInstructions(programBytes)
+    fun decompile(programBytes: ByteArray, outFileName: String) {
+        File(outFileName).printWriter().use { writer ->
+            val intInstructions = parseIntInstructions(programBytes)
 
-        var index = 0
+            var index = 0
 
-        while (index < intInstructions.size) {
-            val intInstruction = intInstructions[index]
-            val opCode = OpCode.fromInt(intInstruction)
+            while (index < intInstructions.size) {
+                val intInstruction = intInstructions[index]
+                val opCode = OpCode.fromInt(intInstruction)
 
-            if (opCode != null) {
-                println(opCode)
+                if (opCode != null) {
+                    writer.println(opCode)
 
-                val operandCount = opCode.operandCount
+                    val operandCount = opCode.operandCount
 
-                if (operandCount > 0) {
-                    printOperands(intInstructions.subList(index + 1, index + operandCount + 1))
+                    if (operandCount > 0) {
+                        intInstructions
+                                .subList(index + 1, index + operandCount + 1)
+                                .map { instruction -> Operand.fromInt(instruction) }
+                                .forEach { operand -> writer.println(operand) }
+                    }
+
+                    index += operandCount + 1
+                } else {
+                    // Represents initial data (vs initial instruction) in binary file.
+                    writer.println(intInstruction)
+                    index += 1
                 }
-
-                index += operandCount + 1
-            } else {
-                println(intInstruction)
-                index += 1
             }
         }
-    }
-
-    private fun printOperands(intInstructions: List<Int>) {
-        intInstructions
-                .map { instruction -> Operand.fromInt(instruction) }
-                .forEach { operand -> println(operand) }
     }
 
 }
