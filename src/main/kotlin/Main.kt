02074f1022d7897c0e228a7e6cb19840074834f6
Stuart Kent <skent.dev@gmail.com>
2017-09-15 20:23:07 -0400

    Add action prompt on Main run

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import vm.Decompiler
 import vm.VM
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.*
 
 object Main {
 
@@ -9,8 +10,21 @@ object Main {
     fun main(args: Array<String>) {
         val programBytes = readProgramBytes("challenge.bin")
 
-        Decompiler().decompile(programBytes)
-        VM(actor = InteractiveActor).runProgram(programBytes)
+        println("""
+            Choose an option:
+
+            [1] Run program")
+            [2] Decompile program")
+
+            > """.trimIndent())
+
+        val option = Scanner(System.`in`).nextLine()!!
+
+        when (option) {
+            "1" -> VM(actor = InteractiveActor).runProgram(programBytes)
+            "2" -> Decompiler().decompile(programBytes)
+            else -> println("Option not recognized. Exiting.")
+        }
     }
 
     private fun readProgramBytes(programName: String): ByteArray {
