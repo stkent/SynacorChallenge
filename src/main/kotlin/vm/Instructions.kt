ddf8bf4b73e65344bac9feb8f9df74ae14ab6190
Stuart Kent <skent.dev@gmail.com>
2017-09-22 17:01:46 -0400

    Refactor instructions printing

--- src/main/kotlin/vm/Instructions.kt
+++ src/main/kotlin/vm/Instructions.kt
@@ -33,45 +33,42 @@ fun printOpCodeAndOperands(
 
     writer.println(opCode)
 
-    val operandCount = opCode.operandCount
-    val operands = intInstructions
-            .subList(index + 1, index + operandCount + 1)
+    intInstructions
+            .subList(index + 1, index + opCode.operandCount + 1)
             .map { instruction -> Operand.fromInt(instruction) }
-
-    if (opCode == OpCode.OUT) {
-        // Special handling for OUT because operands will often represent ASCII characters.
-        printOutOperand(operands[0], registers, writer)
-    } else if (operands.isNotEmpty()) {
-        operands.forEach { operand -> printNonOutOperand(operand, registers, writer) }
-    }
+            .forEach { operand -> printOperand(opCode, operand, registers, writer) }
 }
 
-private fun printOutOperand(operand: Operand, registers: IntArray?, writer: PrintWriter) {
+private fun printOperand(opCode: OpCode, operand: Operand, registers: IntArray?, writer: PrintWriter) {
     when (operand) {
-        is Operand.Number -> {
-            val operandChar = operand.value.toChar()
-
-            if (operandChar == '\n') {
-                /*
-                 * Print newline literals (1 line "tall"), rather than inserting actual newlines (2 lines "tall") so
-                 * that our decompiled file line numbers correspond to instruction indices (offset by 1, since the
-                 * instruction indices are 0-indexed but the file line numbers are 1-indexed).
-                 */
-                printIndented("\"\\n\"", writer)
-            } else {
-                printIndented("\"$operandChar\"", writer)
-            }
+        is Operand.Register -> {
+            printRegisterOperand(operand, registers, writer)
         }
 
-        is Operand.Register -> printRegisterOperand(operand, registers, writer)
+        is Operand.Number -> {
+            // Special handling for OUT because operands will often represent ASCII characters.
+            if (opCode == OpCode.OUT) {
+                printOutNumberOperand(operand, writer)
+            } else {
+                printIndented("$operand", writer)
+            }
+        }
     }
 }
 
-private fun printNonOutOperand(operand: Operand, registers: IntArray?, writer: PrintWriter) {
-    when (operand) {
-        is Operand.Number   -> printIndented("$operand", writer)
-        is Operand.Register -> printRegisterOperand(operand, registers, writer)
-    }
+private fun printOutNumberOperand(number: Operand.Number, writer: PrintWriter) {
+        val operandChar = number.value.toChar()
+
+        if (operandChar == '\n') {
+            /*
+             * Print newline literals (1 line "tall"), rather than inserting actual newlines (2 lines "tall") so
+             * that our decompiled file line numbers correspond to instruction indices (offset by 1, since the
+             * instruction indices are 0-indexed but the file line numbers are 1-indexed).
+             */
+            printIndented("\"\\n\"", writer)
+        } else {
+            printIndented("\"$operandChar\"", writer)
+        }
 }
 
 private fun printRegisterOperand(register: Operand.Register, registers: IntArray?, writer: PrintWriter) {
