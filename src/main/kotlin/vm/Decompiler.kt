6c1b950e877697540d99b72ef42f41d915c455e0
Stuart Kent <skent.dev@gmail.com>
2017-09-22 16:21:57 -0400

    Move instruction printing methods

--- src/main/kotlin/vm/Decompiler.kt
+++ src/main/kotlin/vm/Decompiler.kt
@@ -1,6 +1,5 @@
 package vm
 
-import vm.OpCode.OUT
 import java.io.PrintWriter
 
 class Decompiler {
@@ -28,64 +27,6 @@ class Decompiler {
                 index += 1
             }
         }
-    }
-
-    /**
-     * NOTE: intInstructions\[index\] MUST represent an OpCode.
-     */
-    fun printOpCodeAndOperands(
-            index: Int,
-            intInstructions: List<Int>,
-            registers: IntArray? = null,
-            writer: PrintWriter) {
-
-        val intInstruction = intInstructions[index]
-        val opCode = OpCode.fromInt(intInstruction)
-
-        writer.println(opCode)
-
-        val operandCount = opCode.operandCount
-        val operands = intInstructions
-                .subList(index + 1, index + operandCount + 1)
-                .map { instruction -> Operand.fromInt(instruction) }
-
-        if (opCode == OUT) {
-            // Special handling for OUT because operands will often be ASCII characters.
-            printOutOperand(operands[0], registers, writer)
-        } else if (operands.isNotEmpty()) {
-            operands.forEach { operand -> printOperandValue("$operand", writer) }
-        }
-    }
-
-    private fun printOutOperand(operand: Operand, registers: IntArray?, writer: PrintWriter) {
-        when (operand) {
-            is Operand.Number -> {
-                val operandChar = operand.value.toChar()
-
-                if (operandChar == '\n') {
-                    /*
-                     * Print newline literals (1 line "tall"), rather than inserting actual newlines (2 lines "tall") so
-                     * that our decompiled file line numbers correspond to instruction indices (offset by 1, since the
-                     * instruction indices are 0-indexed but the file line numbers are 1-indexed).
-                     */
-                    printOperandValue("\"\\n\"", writer)
-                } else {
-                    printOperandValue("\"$operandChar\"", writer)
-                }
-            }
-
-            is Operand.Register -> {
-                if (registers != null) {
-                    printOperandValue("$operand = ${registers[operand.index]}", writer)
-                } else {
-                    printOperandValue("$operand", writer)
-                }
-            }
-        }
-    }
-
-    private fun printOperandValue(literal: String, writer: PrintWriter) {
-        writer.println("  $literal")
     }
 
 }
