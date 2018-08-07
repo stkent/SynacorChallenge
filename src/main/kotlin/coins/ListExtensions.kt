85eb7c45c10969ecfe402cb5acdc0da4d632759f
Stuart Kent <skent.dev@gmail.com>
2018-08-06 20:36:52 -0400

    Formatting fixes

--- src/main/kotlin/coins/ListExtensions.kt
+++ src/main/kotlin/coins/ListExtensions.kt
@@ -1,10 +1,17 @@
 // From https://github.com/MarcinMoskala/KotlinDiscreteMathToolkit
 fun <T> List<T>.permutations(): Set<List<T>> = when {
-  isEmpty() -> setOf()
-  size == 1 -> setOf(listOf(first()))
+  isEmpty() -> {
+    setOf()
+  }
+
+  size == 1 -> {
+    setOf(listOf(first()))
+  }
+
   else -> {
     val element = first()
-    drop(1).permutations()
+    drop(1)
+        .permutations()
         .flatMap { sublist -> (0..sublist.size).map { i -> sublist.plusAt(i, element) } }
         .toSet()
   }
@@ -13,7 +20,7 @@ fun <T> List<T>.permutations(): Set<List<T>> = when {
 // From https://github.com/MarcinMoskala/KotlinDiscreteMathToolkit
 fun <T> List<T>.plusAt(index: Int, element: T): List<T> = when (index) {
   !in 0..size -> throw Error("Cannot put at index $index because size is $size")
-  0 -> listOf(element) + this
-  size -> this + element
-  else -> dropLast(size - index) + element + drop(index)
+  0           -> listOf(element) + this
+  size        -> this + element
+  else        -> dropLast(size - index) + element + drop(index)
 }
